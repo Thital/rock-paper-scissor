49a8d14aaec8a6075b71438fdeff92eb429b7529
thital <mrtroll152@gmail.com>
2023-11-20 17:07:18 +0100

    Now it plays 5 games of rps and reports a winner!

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 choices = ["rock","paper","scissors"];
+let computerScore = 0;
+let playerScore = 0;
 
-let playerSelection = prompt("Choose a hand to play:");
-playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection)
+function getPlayerChoice(){
+    let playerSelection = prompt("Choose a hand to play:");
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    return playerSelection;
+}
 
-let computerSelection
 function getComputerChoice () {
     let random_num = Math.floor(Math.random()*3);
-    computerSelection = choices[random_num];
+    let computerSelection = choices[random_num];
     console.log(computerSelection)
     return computerSelection;
 }
-getComputerChoice()
 
 
 function playRound(playerSelection, computerSelection){
@@ -19,19 +22,43 @@ function playRound(playerSelection, computerSelection){
         case playerSelection === computerSelection:
             return "It's a tie";
         case playerSelection === "rock" && computerSelection == "paper":
+            computerScore += 1;
             return "You lose! Paper beats Rock";
-        case playerSelection === "rock" && computerSelection == "scissors":
+        case playerSelection === "rock" && computerSelection == "scissors":    
+            playerScore +=1;
             return "You win! Rock beats Scissors";
         case playerSelection === "paper" && computerSelection == "rock":
+            playerScore +=1;
             return "You win! Paper beats Rock";
         case playerSelection === "paper" && computerSelection == "scissors":
+            computerScore +=1;
             return "You lose! Scissors beat Paper"; 
         case playerSelection === "scissors" && computerSelection == "rock":
+            computerScore +=1;
             return "You lose! Rock beat Scissors";
         case playerSelection === "scissors" && computerSelection == "paper":
+            playerScore +=1;
             return "You win! Scissors beat Paper";  
         default:
             return "Incorrect input, type in either 'Rock', 'Paper' or 'Scissors'";
     }       
 }
-console.log(playRound(playerSelection, computerSelection))
+
+function game(){
+    for (let i =  0; i < 5; i++){
+        playRound(getPlayerChoice(), getComputerChoice())
+
+    }
+    if (playerScore === computerScore){
+        console.log("It's a tie!")
+    }
+    else if (playerScore > computerScore){
+        console.log("You win!")
+    }
+    else{
+       console.log("You lose!")
+    }
+}
+game()
+console.log(`Your score is ${playerScore}`)
+console.log(`Computer score is ${computerScore}`)
